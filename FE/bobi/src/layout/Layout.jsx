b4a337cc02f961d7097ca947db50dd8b0830a797
SSAFY <sunyeong0412@naver.com>
2022-07-25 14:55:44 +0900

    220725 [#89] Install react-google-login - type: FEAT - react-google-login 설치 후 커밋 (for package.json)

--- FE/bobi/src/layout/Layout.jsx
+++ FE/bobi/src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
+import Footer from "./Footer/Footer";
+import Header from "./Header";
 
 function Layout(props) {
   return (
