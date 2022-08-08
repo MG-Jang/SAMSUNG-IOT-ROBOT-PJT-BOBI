519bc8ceaa57ca87f986cfb3c700ee2bfe91b46a
SH <shhh1006@daum.net>
2022-08-09 08:22:29 +0900

    220809 [#243] Build login button link from FE to BE - type : FEAT - Change window.location.href to BE allauth google login page

--- FE/bobi_frontend/src/pages/Login.jsx
+++ FE/bobi_frontend/src/pages/Login.jsx
@@ -5,10 +5,11 @@ import React from "react";
 function Login() {
   function loginButttonClick(e) {
     window.location.href = "https://accounts.google.com/o/oauth2/auth" +
-    "/identifier?client_id=243625053777-t2htd7u0v85i9fnp0oq0cts7a3ba8tld.apps.googleusercontent.com" +
-    "&redirect_uri=http://127.0.0.1:3000/loginSuccess/" +
+    "/identifier?client_id=243625053777-t2htd7u0v85i9fnp0oq0cts7a3ba8tld.apps.googleusercontent.com" + 
+    "&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fgoogle%2Fgoogle%2Flogin%2Fcallback%2F" +
     "&scope=email" +
-    "&response_type=token" 
+    "&response_type=code" +
+    "&state=Av03K0D4w2WO&flowName=GeneralOAuthFlow"
     
     const parsedHash = new URLSearchParams(window.location.hash.substring(1));
     const accessToken = parsedHash.get("access_token");
