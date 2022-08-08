e680129768b99a01ad88be1831c184e5d131fc10
SH <shhh1006@daum.net>
2022-08-08 11:08:13 +0900

    220808 [#243] Build login at FE - type : FEAT - Build login at FE and connect BE login. Set Redirect URI to FE

--- FE/bobi_frontend/src/pages/Login.jsx
+++ FE/bobi_frontend/src/pages/Login.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+// import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 function Login() {
-    return (
-        <div>
-            <h1>[Login]</h1>
-        </div>
-    )
+  function loginButttonClick(e) {
+    window.location.href = "https://accounts.google.com/o/oauth2/auth/identifier?client_id=243625053777-t2htd7u0v85i9fnp0oq0cts7a3ba8tld.apps.googleusercontent.com&redirect_uri=http://127.0.0.1:3000/&scope=profile%20email&response_type=code&state=md23KmNHs6qM&flowName=GeneralOAuthFlow"
+  }
+  return (
+    <>
+      <button type="button" onClick={loginButttonClick}>login</button>
+      {/* <GoogleLogin
+        clientId={clientId}
+        responseType="id_token"
+        buttonText="구글 로그인 하기"
+        onSuccess={handleSuccessLogin}
+        onFailure={handleFailureLogin}/>
+
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="로그아웃"
+        onLogoutSuccess={handleSuccessLogout} /> */}
+    </>
+  )
 };
 
 export default Login;
