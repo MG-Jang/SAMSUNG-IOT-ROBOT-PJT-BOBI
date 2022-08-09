9926e822fc6a27481a396dad41de3ea430c05525
SSAFY <kdw324400@gmail.com>
2022-08-09 17:32:46 +0900

    220809 [#233] Send MQTT mssg to server
    - type: FEAT
    - Send MQTT mssg to server when voice mssg is uploaded to S3
        - userid/voice/toweb
        - test done in windows

--- HW/VOICE_DETECTION/s3_voice_mssg.py
+++ HW/VOICE_DETECTION/s3_voice_mssg.py
@@ -1,42 +1,45 @@
+from asyncio import open_connection
 import logging
 import boto3
 from botocore.exceptions import ClientError
 import os
+import paho.mqtt.client as mqtt
 
-_bucket_name = "bobivoicebucket"
-_object_name = "ssafy_iot_bobi (1).wav"
-_file_name = "ssafy_iot_bobi (1).wav"
 
 class VoiceMessage():
     def __init__(self):
         super(VoiceMessage, self).__init__()
         self.s3_client = boto3.client('s3')
         self.bucket_name = "bobivoicebucket"
-    
-    def upload_file(self, file_name):
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.connect("i7a208.p.ssafy.io", 1883, 60)
+
+    def upload_file(self, file_name, userid):
         """Upload a file to an S3 bucket
 
         :param file_name: File to upload
-        :param bucket: Bucket to upload to
-        :param object_name: S3 object name. If not specified then file_name is used
+        :param userid: user id for mqtt
         :return: True if file was uploaded, else False
         """
         object_name = os.path.basename(file_name)
 
         try:
-            response = self.s3_client.upload_file(file_name, self.bucket_name, object_name)
+            response = self.s3_client.upload_file(
+                file_name, self.bucket_name, object_name)
         except ClientError as e:
             logging.error(e)
             print("upload failed")
             return False
         print("upload success")
+        topic = userid + "/voice/toweb"
+        self.mqtt_client.publish(topic, "upload")
         return True
 
     def download_file(self, file_name):
         object_name = os.path.basename(file_name)
         self.s3_client.download_file(self.bucket_name, object_name, file_name)
 
+
 """ voice_mssg = VoiceMessage()
 #voice_mssg.download_file(_file_name)
 voice_mssg.upload_file(_file_name) """
-
