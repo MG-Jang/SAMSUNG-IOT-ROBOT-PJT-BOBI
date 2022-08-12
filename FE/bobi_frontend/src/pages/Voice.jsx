c4980c0bb982c2dd638b4aafb4ff06f919c6dbea
SH <shhh1006@daum.net>
2022-08-12 16:30:18 +0900

    220812 [#242] Rendering VoicePlay.jsx - type : FEAT - Import VoicePlay.jsx at Voice.jsx

--- FE/bobi_frontend/src/pages/Voice.jsx
+++ FE/bobi_frontend/src/pages/Voice.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import VoiceRecord from "../components/voice/VoiceRecord";
+import VoicePlay from "../components/voice/VoicePlay";
 
 function Voice () {
 
   return (
     <>
-      <VoiceRecord/>
+      <VoiceRecord />
+      <VoicePlay />
     </>
   )
 };
