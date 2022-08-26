3d6b9cf56352aa300dd63a574ad3a7665c590155
김동원 <kdw324400@gmail.com>
2022-08-27 00:12:52 +0900

    remove key

--- HW/VOICE_DETECTION/voice.sh
+++ HW/VOICE_DETECTION/voice.sh
@@ -4,4 +4,4 @@
 cd ~/google_stt
 source env/bin/activate
 cd ~/WAVEGO/RPi
-python3 voice.py --access_key YN4dbG65PZ6tFVT+e0w1eAMCE2IL0YoDDY7b+yrtGDn9o1N5W+P2HQ== --keyword_paths /home/pi/WAVEGO/RPi/voice_data/hi_bobi_ko_rpi.ppn --model_path /home/pi/WAVEGO/RPi/voice_data/porcupine_params_ko.pv --audio_device_index $1 --user_id $2
+python3 voice.py --access_key [porcupine key] --keyword_paths /home/pi/WAVEGO/RPi/voice_data/hi_bobi_ko_rpi.ppn --model_path /home/pi/WAVEGO/RPi/voice_data/porcupine_params_ko.pv --audio_device_index $1 --user_id $2
